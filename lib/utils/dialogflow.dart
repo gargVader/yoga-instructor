e220e6072c6efa6673e4bb3d03e62326d95f59cf
Souvik Biswas <sbis1999@gmail.com>
2021-03-28 09:28:57 +0530

    Update some dialogflow methods :rocket:

--- lib/utils/dialogflow.dart
+++ lib/utils/dialogflow.dart
@@ -3,13 +3,46 @@ import 'dart:typed_data';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:dialog_flowtter/dialog_flowtter.dart' as df;
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class Dialogflow {
-  static Future<void> initialize() async {
+  static stt.SpeechToText speech = stt.SpeechToText();
+  static bool isSpeechAvailable;
+
+  static initializeSpeech() async {
+    bool available = await speech.initialize(
+      onStatus: (status) => print(status),
+      onError: (error) => print(error.errorMsg),
+    );
+
+    isSpeechAvailable = available;
+  }
+
+  static Future<String> takeVoiceInput() async {
+    String recognizedVoiceInputString = '';
+
+    if (isSpeechAvailable) {
+      await speech.listen(
+        listenFor: Duration(minutes: 2),
+        pauseFor: Duration(seconds: 5),
+        onResult: (result) {
+          double confidence = result.confidence;
+
+          if (confidence > 0.5) {
+            result.recognizedWords;
+            recognizedVoiceInputString = result.recognizedWords;
+          }
+        },
+      );
+    }
+  }
+
+  static Future<df.DetectIntentResponse> initialize() async {
     final df.DialogFlowtter dialogFlowtter = await df.DialogFlowtter.fromFile(
       path: 'assets/dialogflow/sofia_auth.json',
-      sessionId: '101',
+      sessionId: '101', // use the timestamp as the session id
     );
 
     final df.QueryInput queryInput = df.QueryInput(
@@ -31,10 +64,11 @@ class Dialogflow {
       ),
     );
 
-    String audioBase64 = response.outputAudio;
-    Uint8List audioBytes = response.outputAudioBytes;
+    return response;
+  }
 
+  static Future<void> playSpeech({@required Uint8List audioBytes}) async {
     AudioPlayer audioPlayer = AudioPlayer();
-    int result = await audioPlayer.playBytes(audioBytes);
+    await audioPlayer.playBytes(audioBytes);
   }
 }
