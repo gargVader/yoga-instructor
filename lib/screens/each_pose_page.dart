9b77af5ee482c88b280dee40eb4cb5b3f8f283ed
Souvik Biswas <sbis1999@gmail.com>
2021-03-28 09:47:46 +0530

    Add back button on pose page :blue_heart:

--- lib/screens/each_pose_page.dart
+++ lib/screens/each_pose_page.dart
@@ -74,205 +74,220 @@ class _EachPosePageState extends State<EachPosePage> {
                   poses: poses,
                   currentIndex: currentIndex,
                 ),
-      body: SingleChildScrollView(
-        physics: PageScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: screenWidth * 0.8,
-              // child: Image.asset(
-              //   'assets/images/triangle.png',
-              //   fit: BoxFit.fitHeight,
-              // ),
-              child: poseImageUrl != null
-                  ? CachedNetworkImage(
-                      imageUrl: poseImageUrl,
-                      placeholder: (context, url) => Container(
-                        width: double.maxFinite,
-                        height: screenWidth * 0.8,
-                      ),
-                      errorWidget: (context, url, error) => Container(),
-                      fit: BoxFit.fitHeight,
-                    )
-                  : Image.asset(
-                      'assets/images/triangle.png',
-                      fit: BoxFit.fitHeight,
-                    ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            physics: PageScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 16.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(
-                        Icons.access_time,
-                        color: Palette.black.withOpacity(0.8),
-                      ),
-                      SizedBox(width: 8.0),
-                      Text(
-                        '3 minutes',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: 1,
-                          color: Palette.black,
+                SizedBox(
+                  height: screenWidth * 0.8,
+                  // child: Image.asset(
+                  //   'assets/images/triangle.png',
+                  //   fit: BoxFit.fitHeight,
+                  // ),
+                  child: poseImageUrl != null
+                      ? CachedNetworkImage(
+                          imageUrl: poseImageUrl,
+                          placeholder: (context, url) => Container(
+                            width: double.maxFinite,
+                            height: screenWidth * 0.8,
+                          ),
+                          errorWidget: (context, url, error) => Container(),
+                          fit: BoxFit.fitHeight,
+                        )
+                      : Image.asset(
+                          'assets/images/triangle.png',
+                          fit: BoxFit.fitHeight,
                         ),
-                      ),
-                    ],
-                  ),
                 ),
-                InkWell(
-                  onTap: () {
-                    print('Play button tapped !');
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => PreviewScreen(
-                          pose: currentPose,
-                        ),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Palette.lightDarkShade,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(8.0),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 8.0,
-                        right: 16.0,
-                        top: 8.0,
-                        bottom: 8.0,
-                      ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           Icon(
-                            Icons.play_arrow,
-                            color: Colors.white,
-                            size: 36.0,
+                            Icons.access_time,
+                            color: Palette.black.withOpacity(0.8),
                           ),
+                          SizedBox(width: 8.0),
                           Text(
-                            'Play',
-                            // maxLines: 1,
-                            softWrap: true,
-                            overflow: TextOverflow.fade,
+                            '3 minutes',
                             style: TextStyle(
-                              fontSize: 22.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w400,
+                              letterSpacing: 1,
+                              color: Palette.black,
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 16.0,
-                left: 16.0,
-                right: 16.0,
-              ),
-              child: Text(
-                poseNameDisplay,
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  color: Palette.black,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 8.0,
-                left: 16.0,
-                right: 16.0,
-              ),
-              child: Text(
-                poseSubtitle,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w400,
-                  color: Palette.black.withOpacity(0.6),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 16.0,
-                left: 16.0,
-                right: 16.0,
-              ),
-              child: Text(
-                'Some of the benefits of the $poseNameDisplay pose are as follows:',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600,
-                  letterSpacing: 0.6,
-                  color: Palette.black,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 16.0,
-                left: 16.0,
-                right: 16.0,
-              ),
-              child: ListView.builder(
-                primary: false,
-                shrinkWrap: true,
-                itemCount: benefitList.length,
-                padding: EdgeInsets.all(0.0),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(
-                      // top: 8.0,
-                      bottom: 16.0,
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '•  ',
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w800,
-                            letterSpacing: 0.6,
-                            color: Palette.black,
-                          ),
-                        ),
-                        Flexible(
-                          child: Text(
-                            '${benefitList[index]}',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 0.6,
-                              color: Palette.lightDarkShade,
+                    InkWell(
+                      onTap: () {
+                        print('Play button tapped !');
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => PreviewScreen(
+                              pose: currentPose,
                             ),
                           ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Palette.lightDarkShade,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(8.0),
+                          ),
                         ),
-                      ],
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 8.0,
+                            right: 16.0,
+                            top: 8.0,
+                            bottom: 8.0,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.play_arrow,
+                                color: Colors.white,
+                                size: 36.0,
+                              ),
+                              Text(
+                                'Play',
+                                // maxLines: 1,
+                                softWrap: true,
+                                overflow: TextOverflow.fade,
+                                style: TextStyle(
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                  );
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 16.0,
+                    left: 16.0,
+                    right: 16.0,
+                  ),
+                  child: Text(
+                    poseNameDisplay,
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                      color: Palette.black,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 8.0,
+                    left: 16.0,
+                    right: 16.0,
+                  ),
+                  child: Text(
+                    poseSubtitle,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w400,
+                      color: Palette.black.withOpacity(0.6),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 16.0,
+                    left: 16.0,
+                    right: 16.0,
+                  ),
+                  child: Text(
+                    'Some of the benefits of the $poseNameDisplay pose are as follows:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.6,
+                      color: Palette.black,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 16.0,
+                    left: 16.0,
+                    right: 16.0,
+                  ),
+                  child: ListView.builder(
+                    primary: false,
+                    shrinkWrap: true,
+                    itemCount: benefitList.length,
+                    padding: EdgeInsets.all(0.0),
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                          // top: 8.0,
+                          bottom: 16.0,
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '•  ',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w800,
+                                letterSpacing: 0.6,
+                                color: Palette.black,
+                              ),
+                            ),
+                            Flexible(
+                              child: Text(
+                                '${benefitList[index]}',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: 0.6,
+                                  color: Palette.lightDarkShade,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 50.0),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SafeArea(
+              child: IconButton(
+                icon: Icon(Icons.arrow_back_ios),
+                onPressed: () {
+                  Navigator.of(context).pop();
                 },
               ),
             ),
-            SizedBox(height: 50.0),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
