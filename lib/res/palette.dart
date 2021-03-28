99dc9ec6ae6d10888fe6b0af2278cbddc417b78d
Souvik Biswas <sbis1999@gmail.com>
2021-03-28 19:49:40 +0530

    Update palette to const :rocket:

--- lib/res/palette.dart
+++ lib/res/palette.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  static final someColor = Colors.white;
-  static final darkShade = Color(0xFF640226);
-  static final lightDarkShade = Color(0xFFcd3871);
-  static final mediumShade = Color(0xFFfbe1e0);
-  static final lightShade = Color(0xFFFCE0DF);
-  static final black = Color(0xFF230107);
-  static final loginBackground = Color(0xFFffead7);
-  static final nameBackground = Color(0xFFffe6e1);
-  static final genderBackground = Color(0xFF5cb798);
-  static final ageBackground = Color(0xFFfeafb6);
-  static final accentDarkPink = Color(0xFFed576a);
+  static const someColor = Colors.white;
+  static const darkShade = Color(0xFF640226);
+  static const lightDarkShade = Color(0xFFcd3871);
+  static const mediumShade = Color(0xFFfbe1e0);
+  static const lightShade = Color(0xFFFCE0DF);
+  static const black = Color(0xFF230107);
+  static const loginBackground = Color(0xFFffead7);
+  static const nameBackground = Color(0xFFffe6e1);
+  static const genderBackground = Color(0xFF5cb798);
+  static const ageBackground = Color(0xFFfeafb6);
+  static const accentDarkPink = Color(0xFFed576a);
 }
