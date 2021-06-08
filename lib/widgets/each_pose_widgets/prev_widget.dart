de633160be78fe6b1e413531cabee8da52903164
Souvik Biswas <sbis1999@gmail.com>
2021-06-08 10:24:43 +0530

    Fix status bar color - prev widget

--- lib/widgets/each_pose_widgets/prev_widget.dart
+++ lib/widgets/each_pose_widgets/prev_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sofia/model/pose.dart';
 import 'package:sofia/res/palette.dart';
 import 'package:sofia/screens/each_pose_page.dart';
@@ -39,6 +40,10 @@ class PrevWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
+        SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark,
+        ));
         Navigator.of(context).pushReplacement(_routeToPreviousScreen());
       },
       child: Container(
