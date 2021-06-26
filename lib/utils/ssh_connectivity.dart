e307bdf6e6262028e5c96b30b5b0d9ba362d1058
Souvik Biswas <sbis1999@gmail.com>
2021-06-26 13:42:26 +0530

    Add SSH connectivity class

---
+++ lib/utils/ssh_connectivity.dart
@@ -0,0 +1,37 @@
+import 'package:ssh/ssh.dart';
+
+import '../secrets.dart';
+
+class SSHConnectivity {
+  SSHClient client = SSHClient(
+    host: PiConfig.hostname,
+    username: PiConfig.username,
+    port: PiConfig.port,
+    passwordOrKey: PiConfig.password,
+  );
+
+  checkAvailability({Function onReceive}) async {
+    try {
+      await client.connect();
+      print("shell connected!");
+
+      try {
+        await client.startShell(
+          ptyType: "vanilla",
+          callback: (dynamic res) async {
+            String output = res;
+            if (!output.contains("pi@raspberrypi:")) {
+              onReceive(output);
+            }
+          },
+        );
+        print("shell started!");
+        await client.writeToShell("cd ~ && ./oak_check.sh\n");
+      } catch (e) {
+        onReceive("Failed to connect to the device");
+      }
+    } catch (e) {
+      onReceive("Couldn't find the device");
+    }
+  }
+}
