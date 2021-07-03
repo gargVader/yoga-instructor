da4e88b70e80d6d22826bff31b364d7376809d60
Souvik Biswas <sbis1999@gmail.com>
2021-07-03 14:23:51 +0530

    Add subtle animation to each bar

--- lib/widgets/profile_screen/chart_widget.dart
+++ lib/widgets/profile_screen/chart_widget.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+import 'package:supercharged/supercharged.dart';
 
 import 'package:sofia/model/attempts.dart';
 import 'package:sofia/res/palette.dart';
+
+enum AnimProps {
+  bar1,
+  bar2,
+  bar3,
+  bar4,
+  bar5,
+  bar6,
+  bar7,
+}
 
 class ChartWidget extends StatefulWidget {
   final List<Attempt> attempts;
@@ -12,7 +24,11 @@ class ChartWidget extends StatefulWidget {
   _ChartWidgetState createState() => _ChartWidgetState();
 }
 
-class _ChartWidgetState extends State<ChartWidget> {
+class _ChartWidgetState extends State<ChartWidget>
+    with TickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<TimelineValue<AnimProps>> _animation;
+
   List<int> durationList = [0, 0, 0, 0, 0, 0, 0];
   int maxDuration = 1;
 
@@ -34,6 +50,81 @@ class _ChartWidgetState extends State<ChartWidget> {
     // Finding the max value from the list to measure the realative bar heights
     maxDuration =
         durationList.reduce((curr, next) => curr > next ? curr : next);
+
+    _animationController = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _animation = TimelineTween<AnimProps>()
+        .addScene(
+          begin: 0.milliseconds,
+          end: 1.seconds,
+          curve: Curves.easeOut,
+        )
+        .animate(
+          AnimProps.bar1,
+          tween: Tween(begin: 0.0, end: durationList[0]),
+        )
+        .addSubsequentScene(
+          duration: 1.seconds,
+          curve: Curves.easeOut,
+        )
+        .animate(
+          AnimProps.bar2,
+          tween: Tween(begin: 0.0, end: durationList[1]),
+        )
+        .addSubsequentScene(
+          duration: 1.seconds,
+          curve: Curves.easeOut,
+        )
+        .animate(
+          AnimProps.bar3,
+          tween: Tween(begin: 0.0, end: durationList[2]),
+        )
+        .addSubsequentScene(
+          duration: 1.seconds,
+          curve: Curves.easeOut,
+        )
+        .animate(
+          AnimProps.bar4,
+          tween: Tween(begin: 0.0, end: durationList[3]),
+        )
+        .addSubsequentScene(
+          duration: 1.seconds,
+          curve: Curves.easeOut,
+        )
+        .animate(
+          AnimProps.bar5,
+          tween: Tween(begin: 0.0, end: durationList[4]),
+        )
+        .addSubsequentScene(
+          duration: 1.seconds,
+          curve: Curves.easeOut,
+        )
+        .animate(
+          AnimProps.bar6,
+          tween: Tween(begin: 0.0, end: durationList[5]),
+        )
+        .addSubsequentScene(
+          duration: 1.seconds,
+          curve: Curves.easeOut,
+        )
+        .animate(
+          AnimProps.bar7,
+          tween: Tween(begin: 0.0, end: durationList[6]),
+        )
+        .parent
+        .animatedBy(_animationController);
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+
+    super.dispose();
   }
 
   @override
@@ -63,26 +154,18 @@ class _ChartWidgetState extends State<ChartWidget> {
       children: [
         Container(
           width: chartWidth,
-          // height: chartHeight,
+          height: chartHeight + 2,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: durationList.map((bar) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Palette.accentDarkPink,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20.0),
-                        topRight: Radius.circular(20.0),
-                      ),
-                    ),
-                    height: chartHeight * (bar / maxDuration),
-                    width: 20,
-                  );
-                }).toList(),
+              AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, child) => _buildAnimation(
+                  context: context,
+                  child: child,
+                  chartHeight: chartHeight,
+                ),
               ),
               Container(
                 height: 2,
@@ -118,6 +201,32 @@ class _ChartWidgetState extends State<ChartWidget> {
           ),
         )
       ],
+    );
+  }
+
+  Widget _buildAnimation({
+    @required BuildContext context,
+    @required Widget child,
+    @required double chartHeight,
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: durationList.asMap().entries.map((entry) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Palette.accentDarkPink,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
+          ),
+          // height: chartHeight * (bar / maxDuration),
+          height: chartHeight *
+              (_animation.value.get(AnimProps.values[entry.key]) / maxDuration),
+          width: 20,
+        );
+      }).toList(),
     );
   }
 }
