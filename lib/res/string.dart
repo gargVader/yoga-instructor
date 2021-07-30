b873144cbeaf2569e1c17e0fae5fdac3fe67e8ae
Souvik Biswas <sbis1999@gmail.com>
2021-07-30 14:47:42 +0530

    Add some strings

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -1,1 +1,18 @@
 const muxStreamUrl = 'https://stream.mux.com/';
+
+// Hive
+// Targets
+const String hiveDuration = 'duration';
+const String hiveAsanas = 'asanas';
+const String hiveAccuracy = 'accuracy';
+// Settings
+const String hiveHoldDuration = 'hold';
+const String hiveCamera = 'camera';
+const String hiveRaspiPath = 'path';
+
+// Camera options
+const String cameraOAK = 'OAK-D';
+const String cameraMobile = 'mobile';
+
+// Raspberry Pi root path of scripts
+const String raspiRootPath = '~/yoga-instructor-oak-server';
