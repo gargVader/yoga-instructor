4ddbf9eea1b3006e786629e5f5594fd16da8e81f
Souvik Biswas <sbis1999@gmail.com>
2021-07-30 14:46:54 +0530

    Initialize hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
 import 'package:sofia/screens/onboarding_screen.dart';
 import 'package:sofia/screens/onboarding_screens/oak_d_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 /// The list of camera types (mainly including: front and back)
 List<CameraDescription> cameras = [];
 void main() async {
   try {
+    // Initialize Hive
+    await Hive.initFlutter();
+    // Opening the box
+    await Hive.openBox('config');
     // To load the cameras before the app is initialized
     WidgetsFlutterBinding.ensureInitialized();
     cameras = await availableCameras();
