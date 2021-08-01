047e6baf9ebacc0cfe801b8ec2c40614672d2b9f
Souvik Biswas <sbis1999@gmail.com>
2021-08-01 17:53:04 +0530

    User notifier also gets attempts now

--- lib/application/states/retrieve_user_state.dart
+++ lib/application/states/retrieve_user_state.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:sofia/model/attempts.dart';
 import 'package:sofia/model/user.dart';
 
 part 'retrieve_user_state.freezed.dart';
@@ -7,8 +8,13 @@ part 'retrieve_user_state.freezed.dart';
 abstract class RetrieveUserState with _$RetrieveUserState {
   const factory RetrieveUserState() = InitialUserData;
   const factory RetrieveUserState.retrieving() = RetrievingUser;
-  const factory RetrieveUserState.retrieved(User user) = RetrievedUser;
-  const factory RetrieveUserState.hasAccuracyData(User user) =
-      RetrievedUserAccuracy;
+  const factory RetrieveUserState.retrieved(
+    User user,
+    List<Attempt> attempts,
+  ) = RetrievedUser;
+  const factory RetrieveUserState.hasAccuracyData(
+    User user,
+    List<Attempt> attempts,
+  ) = RetrievedUserAccuracy;
   const factory RetrieveUserState.error({String message}) = ErrorRetrievingUser;
 }
