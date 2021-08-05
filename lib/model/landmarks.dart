035723405607a621f750df074faa87a48673b10e
Souvik Biswas <sbis1999@gmail.com>
2021-08-05 23:42:03 +0530

    Add landmark class

---
+++ lib/model/landmarks.dart
@@ -0,0 +1,35 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'landmarks.g.dart';
+
+@JsonSerializable()
+class Landmarks {
+  Landmarks({
+    this.landmarks,
+  });
+
+  List<Landmark> landmarks;
+
+  factory Landmarks.fromJson(Map<String, dynamic> json) =>
+      _$LandmarksFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LandmarksToJson(this);
+}
+
+@JsonSerializable()
+class Landmark {
+  Landmark({
+    this.name,
+    this.x,
+    this.y,
+  });
+
+  String name;
+  double x;
+  double y;
+
+  factory Landmark.fromJson(Map<String, dynamic> json) =>
+      _$LandmarkFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LandmarkToJson(this);
+}
---
+++ lib/model/landmarks.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'landmarks.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Landmarks _$LandmarksFromJson(Map<String, dynamic> json) {
+  return Landmarks(
+    landmarks: (json['landmarks'] as List)
+        ?.map((e) =>
+            e == null ? null : Landmark.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$LandmarksToJson(Landmarks instance) => <String, dynamic>{
+      'landmarks': instance.landmarks,
+    };
+
+Landmark _$LandmarkFromJson(Map<String, dynamic> json) {
+  return Landmark(
+    name: json['name'] as String,
+    x: (json['x'] as num)?.toDouble(),
+    y: (json['y'] as num)?.toDouble(),
+  );
+}
+
+Map<String, dynamic> _$LandmarkToJson(Landmark instance) => <String, dynamic>{
+      'name': instance.name,
+      'x': instance.x,
+      'y': instance.y,
+    };
