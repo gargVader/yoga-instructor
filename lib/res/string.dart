e6a4188e5993cc31ae24fd08701050778233371f
Souvik Biswas <sbis1999@gmail.com>
2021-08-06 10:40:59 +0530

    Add new hive config strings

--- lib/res/string.dart
+++ lib/res/string.dart
@@ -10,6 +10,21 @@ const String hiveHoldDuration = 'hold';
 const String hiveCamera = 'camera';
 const String hiveRaspiPath = 'path';
 
+// Dev Mode
+const String hiveDevMode = 'dev';
+const String hiveSSHConfig = 'ssh';
+
+// SSH
+const String hiveHostName = 'ssh_hostname';
+const String hiveUsername = 'ssh_username';
+const String hivePort = 'ssh_port';
+const String hivePassword = 'ssh_password';
+
+// host: PiConfig.hostname,
+// username: PiConfig.username,
+// port: PiConfig.port,
+// passwordOrKey: PiConfig.password,
+
 // Camera options
 const String cameraOAK = 'OAK-D';
 const String cameraMobile = 'mobile';
