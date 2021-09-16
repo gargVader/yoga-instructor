e9701be901e6f02daac153613953f5a59dc92053
Souvik Biswas <sbis1999@gmail.com>
2021-09-16 23:45:20 +0530

    Add the detector for checking if within frame - 20 frames

--- lib/screens/landmark_mlkit_screen.dart
+++ lib/screens/landmark_mlkit_screen.dart
@@ -1,7 +1,12 @@
 import 'dart:convert';
+import 'dart:math' as math;
 
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:hive/hive.dart';
 import 'package:sofia/model/landmarks.dart';
 import 'package:sofia/model/pose.dart';
@@ -13,9 +18,12 @@ import 'package:sofia/utils/dialogflow.dart';
 // import 'package:sofia/utils/ssh_connectivity.dart';
 import 'package:sofia/utils/video_manager.dart';
 import 'package:sofia/widgets/landmark_oak_widgets/landmark_painter.dart';
+import 'package:sofia/widgets/recognizer_mlkit_screen/painter/pose_painter_mlkit.dart';
 // import 'package:ssh2/ssh2.dart';
 // import 'package:ssh/ssh.dart';
 import 'package:wakelock/wakelock.dart';
+
+import '../main.dart';
 
 // import '../secrets.dart';
 
@@ -37,9 +45,17 @@ class _LandmarkMLKitScreenState extends State<LandmarkMLKitScreen> {
   // SSHConnectivity _sshConnectivity = SSHConnectivity();
   // String landmarks = '';
 
-  // int _insideFrameCount = 0;
+  int _insideFrameCount = 0;
 
-  String _status = 'Initializing OAK-D...';
+  CameraController? _controller;
+  CustomPaint? customPaint;
+  int _cameraIndex = 1;
+  int pointsWithinFrameCount = 0;
+
+  PoseDetector poseDetector = GoogleMlKit.vision.poseDetector();
+  bool isBusy = false;
+
+  String _status = 'Initializing camera...';
   Color _statusColor = Colors.red;
   // bool _isOAKAvailable = true;
   // String? _processId;
@@ -165,19 +181,151 @@ class _LandmarkMLKitScreenState extends State<LandmarkMLKitScreen> {
   //   }
   // }
 
-  testNavigationToRecognizerMLKit() async {
-    await Future.delayed(Duration(seconds: 10));
-    await Navigator.of(context).pushReplacement(
-      PageRouteBuilder(
-        opaque: false,
-        pageBuilder: (context, _, __) => TimerScreen(
-          pose: widget.pose,
-          track: _trackName,
-        ),
-      ),
-      result: 'navigated',
+  Future _startLiveFeed() async {
+    final camera = cameras[_cameraIndex];
+    _controller = CameraController(
+      camera,
+      ResolutionPreset.low,
+      enableAudio: false,
     );
+    _controller?.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      _status =
+          'You are not within the frame of the camera. Please stay in frame while it starts.';
+      _controller?.startImageStream(_processCameraImage);
+      setState(() {});
+    });
   }
+
+  Future _stopLiveFeed() async {
+    await _controller?.stopImageStream();
+    await _controller?.dispose();
+    await poseDetector.close();
+    _controller = null;
+  }
+
+  Future _processCameraImage(CameraImage image) async {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (Plane plane in image.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+
+    final Size imageSize =
+        Size(image.width.toDouble(), image.height.toDouble());
+
+    final camera = cameras[_cameraIndex];
+    final imageRotation =
+        // InputImageRotationMethods.fromRawValue(camera.sensorOrientation) ??
+        InputImageRotation.Rotation_180deg;
+
+    final inputImageFormat =
+        InputImageFormatMethods.fromRawValue(image.format.raw) ??
+            InputImageFormat.NV21;
+
+    final planeData = image.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation,
+      inputImageFormat: inputImageFormat,
+      planeData: planeData,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+    processImage(inputImage);
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (isBusy) return;
+    isBusy = true;
+    final poses = await poseDetector.processImage(
+      inputImage: inputImage,
+      useClassifier: false,
+    );
+    // poses.forEach((pose) {
+    //   print('FF_POSE: ${pose.name}, FF_ACCURACY: ${pose.accuracy}');
+    //   if (_isDetectionAllowed == true) {
+    //     setOakRecognitions(pose.name, pose.accuracy);
+    //   }
+    // });
+
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = PosePainterMLKit(
+        poses: poses,
+        absoluteImageSize: inputImage.inputImageData!.size,
+        rotation: inputImage.inputImageData!.imageRotation,
+        onDraw: (value) {
+          pointsWithinFrameCount = value;
+          // print('POINTS: $value');
+
+          if (pointsWithinFrameCount > 20) {
+            _insideFrameCount++;
+            _status =
+                "You're within the camera frame. Please stay here until it starts";
+            _statusColor = Colors.green;
+          } else {
+            _insideFrameCount = 0;
+            _status =
+                'You are not within the frame of the camera. Please stay in frame while it starts.';
+            _statusColor = Colors.red;
+          }
+
+          if (_insideFrameCount > 20) {
+            _stopLiveFeed();
+            SchedulerBinding.instance?.addPostFrameCallback((_) {
+              Navigator.of(context).pushReplacement(
+                PageRouteBuilder(
+                  opaque: false,
+                  pageBuilder: (context, _, __) => TimerScreen(
+                    pose: widget.pose,
+                    track: _trackName,
+                  ),
+                ),
+                result: 'navigated',
+              );
+            });
+          }
+        },
+      );
+      customPaint = CustomPaint(painter: painter);
+    } else {
+      customPaint = null;
+    }
+
+    isBusy = false;
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  // testNavigationToRecognizerMLKit() async {
+  //   await Future.delayed(Duration(seconds: 10));
+  //   await Navigator.of(context).pushReplacement(
+  //     PageRouteBuilder(
+  //       opaque: false,
+  //       pageBuilder: (context, _, __) => TimerScreen(
+  //         pose: widget.pose,
+  //         track: _trackName,
+  //       ),
+  //     ),
+  //     result: 'navigated',
+  //   );
+  // }
 
   @override
   void initState() {
@@ -192,13 +340,6 @@ class _LandmarkMLKitScreenState extends State<LandmarkMLKitScreen> {
       statusBarColor: Colors.transparent,
     ));
 
-    // _client = SSHClient(
-    //   host: configBox.get(hiveHostName) ?? PiConfig.hostname,
-    //   username: configBox.get(hiveUsername) ?? PiConfig.username,
-    //   port: configBox.get(hivePort) ?? PiConfig.port,
-    //   passwordOrKey: configBox.get(hivePassword) ?? PiConfig.password,
-    // );
-
     Wakelock.enable();
     _trackName = widget.trackName;
 
@@ -206,22 +347,32 @@ class _LandmarkMLKitScreenState extends State<LandmarkMLKitScreen> {
 
     Dialogflow.bodyVisible();
 
-    // Establishing the SSH connection
-    // _sshConnectivity.startLandmarkScript(
-    //   client: _client,
-    //   onReceive: (String output) {
-    //     output = output.trim();
-    //     if (this.mounted) processSSHOutput(output);
-    //   },
-    // );
+    // testNavigationToRecognizerMLKit();
 
-    testNavigationToRecognizerMLKit();
+    // To start the camera feed along with the landmark painter
+    _startLiveFeed();
 
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    // ----------------------------------------------------------
+    // FOR TESTING ONLY - REMOVE THIS <<
+    // ----------------------------------------------------------
+    // SystemChrome.setPreferredOrientations([
+    //   DeviceOrientation.landscapeRight,
+    //   // DeviceOrientation.landscapeLeft,
+    // ]);
+
+    // SystemChrome.setEnabledSystemUIOverlays([]);
+    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    //   statusBarColor: Colors.transparent,
+    // ));
+    // ----------------------------------------------------------
+    // >> FOR TESTING ONLY - REMOVE THIS
+    // ----------------------------------------------------------
+
     double height = MediaQuery.of(context).size.height;
     // double width = MediaQuery.of(context).size.width;
 
@@ -234,49 +385,116 @@ class _LandmarkMLKitScreenState extends State<LandmarkMLKitScreen> {
 
     return WillPopScope(
       onWillPop: () async {
-        // if (_isOAKAvailable)
-        //   _sshConnectivity.stopRecognitionScript(
-        //     processId: _processId,
-        //     client: _client,
-        //   );
+        SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+        ));
+        _stopLiveFeed();
+        // FlutterStatusbarcolor.setStatusBarColor(Colors.transparent);
         return true;
       },
       child: Scaffold(
-        backgroundColor: _statusColor,
+        backgroundColor: Colors.black,
         body: Stack(
           children: [
-            Center(
-              child: CustomPaint(
-                foregroundPainter: LandmarkPainter(
-                  landmarks: _landmarks,
-                  fraction: frac,
-                  color: _statusColor,
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    // border: Border.all(
-                    //   color: Colors.red,
-                    //   width: 5,
-                    // ),
-                  ),
-                  height: containerHeight,
-                  width: containerWidth,
-                ),
-              ),
-            ),
+            mounted && _controller != null
+                ? _controller!.value.isInitialized
+                    ? Center(
+                        child: SizedBox(
+                          height: height,
+                          child: RotatedBox(
+                            quarterTurns: 1,
+                            child: AspectRatio(
+                              aspectRatio: 1 / _controller!.value.aspectRatio,
+                              child: _controller!.buildPreview(),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Container()
+                : Container(),
+            mounted && _controller != null && customPaint != null
+                ? _controller!.value.isInitialized
+                    ? Center(
+                        child: Transform(
+                          alignment: Alignment.center,
+                          transform: Matrix4.rotationY(math.pi),
+                          child: ClipRRect(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: _statusColor,
+                                  width: 4.0,
+                                ),
+                              ),
+                              // color: Colors.black,
+                              // color: Colors.black38,
+                              child: customPaint!,
+                              height: height,
+                              width: height *
+                                  _controller!.value.previewSize!.aspectRatio,
+                              // height: screenSize.height * 0.4,
+                              // width: screenSize.height *
+                              //     0.4 *
+                              //     _controller!.value.aspectRatio,
+                            ),
+                          ),
+                        ),
+                      )
+                    : Container()
+                : Container(),
+            mounted && _controller != null
+                ? _controller!.value.isInitialized
+                    ? Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: _statusColor,
+                              width: 4.0,
+                            ),
+                          ),
+                          child: RotatedBox(
+                            quarterTurns: 1,
+                            child: AspectRatio(
+                              aspectRatio: 1 / _controller!.value.aspectRatio,
+                              child: Container(),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Container()
+                : Container(),
+            // Center(
+            //   child: CustomPaint(
+            //     foregroundPainter: LandmarkPainter(
+            //       landmarks: _landmarks,
+            //       fraction: frac,
+            //       color: _statusColor,
+            //     ),
+            //     child: Container(
+            //       decoration: BoxDecoration(
+            //         color: Colors.black,
+            //         // border: Border.all(
+            //         //   color: Colors.red,
+            //         //   width: 5,
+            //         // ),
+            //       ),
+            //       height: containerHeight,
+            //       width: containerWidth,
+            //     ),
+            //   ),
+            // ),
             Container(
-              color: _statusColor.withOpacity(0.6),
+              color: _statusColor,
               width: double.maxFinite,
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
                 child: Text(
-                  // 'You are not within the frame of the OAK-D camera. Please stay in frame while it starts.',
+                  // 'You are not within the frame of the camera. Please stay in frame while it starts.',
                   _status,
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 16.0,
+                    fontSize: 18.0,
                   ),
                 ),
               ),
